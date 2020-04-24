8d83d1373f12d4bf013762473c069c1612d78b27
MRDGH2821 <mihirrabade@gmail.com>
2020-04-24 17:30:28 +0530

    Did changes in the code
    The code now downloads the files properly, by extracting names and url from Springer Ebooks-converted.xlsx

---
+++ Extras/Books downloader.py
@@ -0,0 +1,17 @@
+import wget
+import xlrd
+
+loc = ("Springer Ebooks-converted.xlsx")
+
+wb = xlrd.open_workbook(loc)
+sheet = wb.sheet_by_index(0)
+
+# For row 0 and column 0
+sheet.cell_value(0, 0)
+
+for i in range(sheet.ncols):
+    data = sheet.row_values(i)
+    url = data[0]
+    name = data[1]
+    print('\nBeginning', name, 'download with wget module')
+    wget.download(url, name)
