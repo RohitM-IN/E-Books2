49b3f045dd59bd0c6298b4585f68917287d9208a
MRDGH2821 <mihirrabade@gmail.com>
2020-04-24 17:30:53 +0530

    Delete Books downloader.py
    Wrong code uploaded and comitted

--- Books downloader.py
+++
@@ -1,6 +0,0 @@
-import wget
-
-print('Beginning file download with wget module')
-
-url = 'http://link.springer.com/content/pdf/10.1007%2F978-3-319-44127-6.pdf'
-wget.download(url, 'Food Analysis Laboratory Manual.pdf')
